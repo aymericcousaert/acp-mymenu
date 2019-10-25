5336cd6784b641ef77ef3b70eecb6e40f31f8a38
Aymeric <aymeric.cousaert@insa-lyon.fr>
2019-10-25 16:57:33 -0300

    done

--- menu/views.py
+++ menu/views.py
@@ -68,6 +68,11 @@ class CreateProductView(LoginRequiredMixin, View):
     def post(self, request):
         form = ProductForm(request.POST)
         if form.is_valid():
+            boolVegetarian, boolGlutenIntolerant = False, False
+            if form.cleaned_data['suitForVegetarian'] == "Si":
+                boolVegetarian = True
+            if form.cleaned_data['suitForGlutenIntolerant'] == "Si":
+                boolGlutenIntolerant = True
             name = form.cleaned_data['category']
             categoryFound = False
             for val in Category.objects.all():
@@ -76,10 +81,15 @@ class CreateProductView(LoginRequiredMixin, View):
                     categoryFound = True
             if categoryFound:
                 Product.objects.create(name=form.cleaned_data['name'], description=form.cleaned_data['description'],
-                                   price=form.cleaned_data['price'], category=category)
+                                       price=form.cleaned_data['price'],
+                                       suitForVegetarian=boolVegetarian,
+                                       suitForGlutenIntolerant=boolGlutenIntolerant,
+                                       category=category)
             else:
                 Product.objects.create(name=form.cleaned_data['name'], description=form.cleaned_data['description'],
-                                   price=form.cleaned_data['price'])
+                                       price=form.cleaned_data['price'],
+                                       suitForVegetarian=boolVegetarian,
+                                       suitForGlutenIntolerant=boolGlutenIntolerant,)
             messages.success(request, 'Producto creado exitosamente')
             return render(request, 'product/create.html', {'form': ProductForm()})
         return render(request, "product/create.html", {'form': form})
@@ -115,7 +125,6 @@ class CategoryListView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return {'categories': Category.objects.all(), 'products': Product.objects.all()}
-
 
 
 # New category view
