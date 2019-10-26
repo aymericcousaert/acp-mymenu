2c7d797fbac94b4087ef2aa4594ea59af5a838b1
Aymeric <aymeric.cousaert@insa-lyon.fr>
2019-10-26 16:22:17 -0300

    done

--- menu/urls.py
+++ menu/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('products/', views.ProductListView.as_view(), name='products'),
     path('product/', views.CreateProductView.as_view(), name='product'),
+    path('product/<int:pk>/description', views.ProductDescription.as_view(), name='productDescription'),
     path('product/<int:pk>/delete', views.DeleteProductView.as_view(), name='product-delete'),
     path('payment/', views.PaymentMethodView.as_view(), name='payment'),
     path('payments/', views.PaymentMethodListView.as_view(), name='payment_list'),
