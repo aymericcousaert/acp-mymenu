512fa08a2276e41b7bed16a92b1b6278a30ca9b3
franhermani <franhermani@gmail.com>
2019-10-29 15:18:46 -0300

    Added client model, it's migration and empty admin action

--- menu/admin.py
+++ menu/admin.py
@@ -7,10 +7,22 @@ from menu.models import PaymentMethod
 from menu.models import Category
 from menu.models import DailySpecial
 from menu.models import Promotion
+from menu.models import Client
+
+
+class ClientAdmin(admin.ModelAdmin):
+    actions = ['send_email']
+
+    def send_email(self, request, queryset):
+        # complete this function
+        a = 10
+
+    send_email.short_description = "Send email to selected clients with suggestions URL"
+
 
 admin.site.register(Product)
 admin.site.register(PaymentMethod)
 admin.site.register(Category)
 admin.site.register(DailySpecial)
 admin.site.register(Promotion)
-
+admin.site.register(Client, ClientAdmin)
