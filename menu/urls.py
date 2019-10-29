30172c123b462c8a26440dcfd14fa1db75781349
Aymeric <aymeric.cousaert@insa-lyon.fr>
2019-10-29 16:27:55 -0300

    visualizacion de promociones done

--- menu/urls.py
+++ menu/urls.py
@@ -6,9 +6,10 @@ urlpatterns = [
     path('', views.Index.as_view(), name='index'),
     path('', include('django.contrib.auth.urls')),
     path('products/', views.ProductListView.as_view(), name='products'),
-path('product/<int:pk>/description', views.ProductDescription.as_view(), name='productDescription'),
+    path('product/<int:pk>/description', views.ProductDescription.as_view(), name='productDescription'),
     path('payments/', views.PaymentMethodListView.as_view(), name='payment_list'),
     path('categories/', views.CategoryListView.as_view(), name='categories'),
+    path('promotions/', views.PromotionListView.as_view(), name='promotions'),
 ]
 
 
